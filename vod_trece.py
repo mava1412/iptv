6d29f087236862f1b4ff94426b438f25ea8db61a
mava1412 <mariano.vazquez@live.com>
2018-05-14 14:41:47 +0000

    Code styling issues

--- vod_trece.py
+++ vod_trece.py
@@ -1,18 +1,10 @@
 from urllib import request
 from bs4 import BeautifulSoup
-import requests
 
 from pyvirtualdisplay import Display
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
-display = Display(visible=0, size=(1920, 1080)).start()
-
-driver = webdriver.Firefox()
-    
 
 def make_soup(url):
   hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
@@ -37,11 +29,11 @@ pages = []
 last_page = page_soup.find('li', class_= 'pager__item pager__item--last').find('a')
 last_page_url = last_page['href']
 last_page_num = int(last_page_url[-1])
- 
+
+
 def find_links():
   l = []
   visited = []
-  #d = {}
   for link in range(last_page_num):
     soup = make_soup(base+capitulos+str(link+1))
     for a in soup.find_all('a'):
@@ -49,46 +41,40 @@ def find_links():
         if '/programas/simona/capitulos-completos/capitulo' in a['href']:#and a['href'] not in visited:       
           l.append(base + a['href'])
           visited.append(a['href'])
-          #d["Dato"] = base + a['href'].strip('#comments')
       except:
         pass
   return(l)
 
-def pull_m3u8(soup):
-  for i in soup.find_all('li', {"class": ["quality", "activeQuality"]}):
-    print(i['data-value'])
 
-  
+def trim_list(lst):
+        final_lst = []
+        for line in lst:
+            if line not in final_lst:
+                final_lst.append(line)
+        return(final_lst)
+
+
 if __name__ == '__main__':
-    
-    #https://api.vodgc.net/player/conf/playerId/PRZ9KU1515679151/contentId/557774
-    #https://vod.vodgc.net/gid1/vod/Artear/Eltrece/47/SIMO899C9A803069DF3C3DA03E7C912DB023208_1080P.mp4
-    #https://vod.vodgc.net/manifest/SIMO899C9A803069DF3C3DA03E7C912DB023208.m3u8
-    
+
+    display = Display(visible=0, size=(1920, 1080)).start()
+
+    driver = webdriver.Firefox()
     lst = find_links()
-#    url = 'https://www.eltrecetv.com.ar/programas/simona/capitulos-completos/capitulo-73-de-simona_101489'
-#    
-#    codigo = make_soup(url)
-#  
-#    with open("output1.html", "w") as file:
-#        file.write(str(codigo))
-    
-    l = []
-    for row in lst:
-        if row not in l:
-            page_source = make_soup(row)
-            episode_number = page_source.find('h1', class_ = 'heading').get_text()
-            episode_name = page_source.find('h3', class_ = 'head-line').get_text()
-            vodgc = ([i['src'] for i in page_source.find_all('iframe')])
-            link = vodgc[1]
-            driver.get(link)
-            driver_source = driver.page_source
-            vod_final = BeautifulSoup(driver_source, 'html.parser')          
-            m3u8 = ([i['data-value'] for i in vod_final.find_all('li', class_ = 'quality activeQuality')])
-            kodi_link = ''.join(m3u8)
-            id = kodi_link.split('/')[4]
-            mp4_link = id.replace('.m3u8', '_1080P.mp4')
-            pre_link = 'https://vod.vodgc.net/gid1/vod/Artear/Eltrece/47/'
-            print("#EXTINF:-1,{} - {}\n{}{}".format(episode_number,episode_name,pre_link,mp4_link))
-    
-    
+
+    final_list = trim_list(lst)
+
+    for row in final_list:
+        page_source = make_soup(row)
+        episode_number = page_source.find('h1', class_ = 'heading').get_text()
+        episode_name = page_source.find('h3', class_ = 'head-line').get_text()
+        vodgc = ([i['src'] for i in page_source.find_all('iframe')])
+        link = vodgc[1]
+        driver.get(link)
+        driver_source = driver.page_source
+        vod_final = BeautifulSoup(driver_source, 'html.parser')          
+        m3u8 = ([i['data-value'] for i in vod_final.find_all('li', class_ = 'quality activeQuality')])
+        kodi_link = ''.join(m3u8).strip()
+        id = kodi_link.split('/')[4]
+        mp4_link = id.replace('.m3u8', '_1080P.mp4')
+        pre_link = 'https://vod.vodgc.net/gid1/vod/Artear/Eltrece/47/'
+        print("#EXTINF:-1,{} - {}\n{}{}".format(episode_number,episode_name,pre_link,mp4_link))
